ed3bfbf42b9107095fb3684fb2e65dbfe7dbf4db
EKettu <elina@Elinas-MacBook-Pro.local>
2018-09-21 15:28:37 +0300

    week 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   include RatingAverage
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
+  has_many :memberships
+  has_many :beer_clubs, through: :memberships
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+
+  password_validator = /((?=.*\d)(?=.*[A-Z]))/x
+  validates :password, length: { minimum: 4 }, format: {
+    with: password_validator,
+    message: "minimun length 4, must contain one number 0-9, and one capital letter"
+  }
 
   has_secure_password
 end
